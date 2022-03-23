1278d9036a149823d4acb054272d87e82339c900
Aadithya Prakash <aadithyaprakash@Aadithyas-MacBook-Pro.local>
2022-03-23 10:54:00 -0700

    Functional IEEE802154 UPIPE Driver and modified STM32L4* DTS files to accomodate NVS

--- drivers/ieee802154/ieee802154_uart_pipe.h
+++ drivers/ieee802154/ieee802154_uart_pipe.h
@@ -1,15 +1,18 @@
-/* ieee802154_uart_pipe.h - Private header for UART PIPE fake radio driver */
-
-/*
- * Copyright (c) 2016 Intel Corporation.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #ifndef ZEPHYR_DRIVERS_IEEE802154_IEEE802154_UART_PIPE_H_
 #define ZEPHYR_DRIVERS_IEEE802154_IEEE802154_UART_PIPE_H_
 
+#include <net/ieee802154_radio.h>
+
 #define UART_PIPE_RADIO_15_4_FRAME_TYPE		0xF0
+
+typedef enum upipe_802154_rx_error_t
+{
+	UPIPE_802154_RX_ERROR_INVALID_FRAME,
+	UPIPE_802154_RX_ERROR_DELAYED_TIMEOUT,
+	UPIPE_802154_RX_ERROR_INVALID_FCS,
+	UPIPE_802154_RX_ERROR_INVALID_DEST_ADDR,
+	UPIPE_802154_RX_ERROR_CATCHALL
+} upipe_802154_rx_error_t;
 
 struct upipe_context {
 	struct net_if *iface;
@@ -21,6 +24,7 @@ struct upipe_context {
 	uint8_t rx_len;
 	uint8_t rx_off;
 	uint8_t rx_buf[127];
+	ieee802154_event_cb_t event_handler;
 };
 
 #endif /* ZEPHYR_DRIVERS_IEEE802154_IEEE802154_UART_PIPE_H_ */
