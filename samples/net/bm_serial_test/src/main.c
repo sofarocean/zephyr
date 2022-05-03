2316b01a0c2b93543a7b875986d8edc5b3b32ee0
beardedtrogon <aadithyaprakash@gmail.com>
2022-05-03 15:37:40 -0700

    Sofar/aadu/manchester (#10)
    Removed COBS encoding, added manchester encoding, and created unit tests

--- samples/net/bm_serial_test/src/main.c
+++ samples/net/bm_serial_test/src/main.c
@@ -14,46 +14,52 @@
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
-
+#include <sys/crc.h>
 #include <logging/log.h>
 #include <drivers/console/bm_serial.h>
 
 #define LOG_MODULE_NAME bm_test
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
 
-#define USER0_NODE DT_ALIAS(user0)
-#define USER1_NODE DT_ALIAS(user1)
+static struct k_thread _rx_thread_data;
+K_THREAD_STACK_DEFINE( _rx_stack, TASK_STACK_SIZE);
 
-/* These map to D3/D4 on the L496ZG Nucleo*/
-static const struct gpio_dt_spec user0 = GPIO_DT_SPEC_GET(USER0_NODE, gpios);
-static const struct gpio_dt_spec user1 = GPIO_DT_SPEC_GET(USER1_NODE, gpios);
+static void _rx_thread(void)
+{
+	struct k_msgq* rx_queue = NULL;
+	bm_msg_t msg;
+	uint16_t frame_length;
+	uint16_t payload_length;
+
+	while (rx_queue == NULL)
+	{
+		rx_queue = bm_serial_get_rx_msgq_handler();
+	} 
+
+	while (1)
+	{
+		// Wait on bm_serial.c RX Thread to put message on queue
+		k_msgq_get(rx_queue, &msg, K_FOREVER);
+
+        frame_length = msg.frame_length;
+		payload_length = frame_length - sizeof(bm_frame_header_t);
+		uint8_t bm_payload_type = ((bm_frame_header_t*) msg.frame_addr)->payload_type;
+
+		if (bm_payload_type != BM_IEEE802154)
+		{
+			LOG_ERR("Incompatible version. Discarding Frame");
+			continue;
+		}
+        else
+        {
+            LOG_INF("%s", &msg.frame_addr[sizeof(bm_frame_header_t)]);
+        }
+    }
+}
 
 void main(void)
 {
     LOG_INF( "Testing the BM Serial driver" );
-
-    /* Checking/Setting up GPIO Debug Pins */
-    if (!device_is_ready(user0.port))
-    {
-        LOG_ERR("GPIO 0 Port not ready");
-    }
-
-    if (!device_is_ready(user1.port))
-    {
-        LOG_ERR("GPIO 1 Port not ready");
-    }
-
-    int ret = gpio_pin_configure_dt(&user0, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) 
-    {
-        LOG_ERR("GPIO 0 unable to be configured\n");
-    }
-
-    ret = gpio_pin_configure_dt(&user1, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) 
-    {
-        LOG_ERR("GPIO 1 unable to be configured\n");
-    }
 
 	bm_serial_init();
 
@@ -61,18 +67,25 @@ void main(void)
     uint8_t msg[] = "Hello World";
     int retval;
     bm_frame_header_t frm_hdr = {BM_V0, BM_IEEE802154, sizeof(msg)};
+    frm_hdr.header_crc = crc16_ccitt(0, (uint8_t *) &frm_hdr, sizeof(bm_frame_header_t) - sizeof(bm_crc_t));
 
     memcpy(tx_buf, &frm_hdr, sizeof(bm_frame_header_t));
 	memcpy(&tx_buf[sizeof(bm_frame_header_t)], msg, frm_hdr.payload_length);
     bm_frame_t *bm_frm = (bm_frame_t *)tx_buf;
 
+    k_thread_create(&_rx_thread_data, _rx_stack,
+		K_THREAD_STACK_SIZEOF(_rx_stack),
+		(k_thread_entry_t) _rx_thread,
+		NULL, NULL, NULL, K_PRIO_COOP(5), 0, K_NO_WAIT);
+
 	while (1)
     {
         retval = bm_serial_frm_put(bm_frm);
+        
 	    if (retval)
 	    {
 		    LOG_ERR( "TX MessageQueue is full, dropping message!");
 	    }
-        usleep(100000UL);
+        usleep(2500UL);
     }
 }
---
+++ tests/drivers/bristlemouth/src/main.c
@@ -0,0 +1,213 @@
+/* main.c - OpenThread NCP */
+
+/*
+ * Copyright (c) 2020 Tridonic GmbH & Co KG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <device.h>
+#include <devicetree.h>
+
+#include <logging/log.h>
+#include <drivers/console/bm_serial.h>
+
+#define LOG_MODULE_NAME bm_test
+LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
+
+/* Basic test with 2 byte payload before delimiter */
+static int basic_test_0 (void)
+{
+    int i;
+    bm_ret_t ret;
+    int success = 0;
+    uint8_t test_buf[] = { 0x55, 0x55, 0X55, 0x55, 0x5A, 0xAA, 0xAA, 0xAA, 0xAA, 0xA9, 0xAA, 0xAA, 0xAA, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
+    uint8_t expected_out[] = {0xAA, 0xAA, 0xAA, 0xAA, 0xA9, 0xAA, 0xAA, 0xAA, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
+
+    for (i = 0; i < sizeof(test_buf); i++)
+    {
+        ret = bm_serial_process_byte(test_buf[i]);
+    }
+
+    if (ret.retval == 0 && ret.length == sizeof(expected_out))
+    {
+        success = 1;
+        for (i = 0; i < sizeof(expected_out); i++)
+        {
+            if (expected_out[i] != ret.buf_ptr[i])
+            {
+                success = 0;
+                break;
+            }
+        }
+    }
+    return success;
+}
+
+/* Missing preamble byte */
+static int basic_test_1 (void)
+{
+    int i;
+    bm_ret_t ret;
+    int success = 0;
+    uint8_t test_buf[] = { 0x55, 0x55, 0X55, 0x5A, 0xAA, 0xAA, 0xAA, 0xAA, 0xA9, 0xAA, 0xAA, 0xAA, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
+
+    for (i = 0; i < sizeof(test_buf); i++)
+    {
+        ret = bm_serial_process_byte(test_buf[i]);
+    }
+
+    if (ret.retval != 0)
+    {
+        success = 1;
+    }
+    return success;
+}
+
+/* Test case with preamble + delimiter within payload in a valid frame*/
+static int basic_test_2 (void)
+{
+    int i;
+    bm_ret_t ret;
+    int success = 0;
+    uint8_t test_buf[] = { 0x55, 0x55, 0X55, 0x55, 0x5A, 0xAA, 0xAA, 0xAA, 0xAA, 0xA5, 0xAA, 0xAA, 0xAA, 0x55, 0x55, 0x55, 0x55, 0x5A, 0x5A, 0x99, 0x99, 0x99, 0x99};
+    uint8_t expected_out[] = {0xAA, 0xAA, 0xAA, 0xAA, 0xA5, 0xAA, 0xAA, 0xAA, 0x55, 0x55, 0x55, 0x55, 0x5A, 0x5A, 0x99, 0x99, 0x99, 0x99};
+
+    for (i = 0; i < sizeof(test_buf); i++)
+    {
+        ret = bm_serial_process_byte(test_buf[i]);
+    }
+
+    if (ret.retval == 0 && ret.length == sizeof(expected_out))
+    {
+        success = 1;
+        for (i = 0; i < sizeof(expected_out); i++)
+        {
+            if (expected_out[i] != ret.buf_ptr[i])
+            {
+                success = 0;
+                break;
+            }
+        }
+    }
+    return success;
+}
+
+/* Missing delimiter byte */
+static int basic_test_3 (void)
+{
+    int i;
+    bm_ret_t ret;
+    int success = 0;
+    uint8_t test_buf[] = { 0x55, 0x55, 0X55, 0x55, 0xAA, 0xAA, 0xAA, 0xAA, 0xA9, 0xAA, 0xAA, 0xAA, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
+
+    for (i = 0; i < sizeof(test_buf); i++)
+    {
+        ret = bm_serial_process_byte(test_buf[i]);
+    }
+
+    if (ret.retval != 0)
+    {
+        success = 1;
+    }
+    return success;
+}
+
+/* Missing delimiter byte and following byte is */
+static int basic_test_4 (void)
+{
+    int i;
+    bm_ret_t ret;
+    int success = 0;
+    uint8_t test_buf[] = { 0x55, 0x55, 0X55, 0x55, 0x5A, 0xAA, 0xAA, 0xAA, 0xA9, 0xAA, 0xAA, 0xAA, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
+
+    for (i = 0; i < sizeof(test_buf); i++)
+    {
+        ret = bm_serial_process_byte(test_buf[i]);
+    }
+
+    if (ret.retval != 0)
+    {
+        success = 1;
+    }
+    return success;
+}
+
+
+void main(void)
+{
+    int ret;
+    LOG_INF( "Testing the BM Serial driver" );
+
+	bm_serial_init();
+
+	while (1)
+    {
+        ret = basic_test_0();
+        if(ret == 1)
+        {
+            printk("Basic Test 0 passed\n");
+        }
+        else
+        {
+            printk("Basic Test 0 failed\n");
+        }
+
+        ret = basic_test_1();
+        if(ret == 1)
+        {
+            printk("Basic Test 1 passed\n");
+        }
+        else
+        {
+            printk("Basic Test 1 failed\n");
+        }
+        
+        ret = basic_test_2();
+        if(ret == 1)
+        {
+            printk("Basic Test 2 passed\n");
+        }
+        else
+        {
+            printk("Basic Test 2 failed\n");
+        }
+
+        ret = basic_test_3();
+        if(ret == 1)
+        {
+            printk("Basic Test 3 passed\n");
+        }
+        else
+        {
+            printk("Basic Test 3 failed\n");
+        }
+
+        ret = basic_test_0();
+        if(ret == 1)
+        {
+            printk("Basic Test 0 passed\n");
+        }
+        else
+        {
+            printk("Basic Test 0 failed\n");
+        }
+
+        ret = basic_test_4();
+        if(ret == 1)
+        {
+            printk("Basic Test 4 passed\n");
+        }
+        else
+        {
+            printk("Basic Test 4 failed\n");
+        }
+
+        while(1);
+    }
+}
